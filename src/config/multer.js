b3b3ef99d7b41b610ee7e509622d0847a68da3f8
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2019-07-03 22:47:34 +0100

    adding FileModel and FileController responsibles for upload of files

---
+++ src/config/multer.js
@@ -0,0 +1,15 @@
+import multer from 'multer';
+import { resolve, extname } from 'path';
+import cypto from 'crypto';
+
+export default {
+  storage: multer.diskStorage({
+    destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    filename: (req, file, cb) => {
+      cypto.randomBytes(16, (err, res) => {
+        if (err) return cb(err);
+        return cb(null, res.toString('hex') + extname(file.originalname));
+      });
+    },
+  }),
+};
